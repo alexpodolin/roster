baa5ca7bd1ebacc2703eec2f4a2a85fe931c66ab
alex_podolin <alex.podolin@gmail.com>
2018-06-25 16:44:39 +0300

    dates

--- app/models.py
+++ app/models.py
@@ -14,11 +14,30 @@ class Users(db.Model):
     name = db.Column(db.String(15), unique=False, nullable=False)    
     patronymic = db.Column(db.String(15), unique=False, nullable=False)
     user_color = db.Column(db.CHAR(length=6), unique=False, nullable=False)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+    phone = db.Column(db.String(12), unique=True, nullable=False)    
+    dates = db.relationship('DutyDates', backref='user', lazy='dynamic')
 
     def __init__(self, *args, **kwargs):
         super(Users, self).__init__(*args, **kwargs)
         
     def __repr__(self):
-        return '<Users surname: {}, name: {}, patronymic: {}, user_color: {}>'\
-                .format(self.surname, self.name, self.patronymic, self.user_color)
+        return '<Users id: {}, surname: {}, name: {}, patronymic: {}, \
+                user_color: {}, email: {}, phone: {}>' \
+                .format(self.id, self.surname, self.name, self.patronymic, \
+                        self.user_color, self.email, self.phone )
                 
+class DutyDates(db.Model):
+    __tablename__ = 'duty_dates'
+    id = db.Column(db.Integer,primary_key=True, autoincrement=True)
+    id_user = db.Column(db.SmallInteger, db.ForeignKey('users.id'))
+    date = db.Column(db.Date, nullable=False)
+    
+    def __init__(self, *args, **kwargs):
+        super(DutyDates, self).__init__(*args, **kwargs)
+        
+    def __repr__(self):
+        return '<DutyDates id: {}, id_user: {}, date: {}>' \
+                .format(self.id, self.id_user, self.date)
+    
+    
--- app/static/js/js_custom.js
+++ app/static/js/js_custom.js
@@ -1,6 +1,12 @@
 // получим значение в таблицы ячейки
-function qwe(el) {
-    var x = el.getAttribute('data-day')
-    //console.log(x)
-    return x;
+function duty_date(el) {
+    var date = el.getAttribute('data-day')
+    //console.log(date)    
+    
+    // Подставим это значение в заголовок модального окна
+    function display_date(date) {        
+        document.getElementById('ModalRosterTitleDate').innerHTML = date;
+    }    
+    display_date(date);
 }
+
